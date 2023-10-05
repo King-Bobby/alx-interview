e44897ebb2911e373db7ce737fea6fc845f6641d
Noel Brown <noelbrowndboss@gmail.com>
2023-10-05 22:37:23 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Contains the function canUnlockAll(boxes)
+"""
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        visited[current_box] = True
+
+        for key in boxes[current_box]:
+            if key >= 0 and key < n and not visited[key]:
+                stack.append(key)
+
+    return all(visited)
