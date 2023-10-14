4112c8fe8351f9c15b65e8e1261349c74fed39dc
Noel Brown <noelbrowndboss@gmail.com>
2023-10-14 15:19:39 +0000

    testing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,20 +5,21 @@ Contains the function canUnlockAll(boxes)
 
 
 def canUnlockAll(boxes):
-    """determines if all the boxes can be opened"""
+    """Checks if all boxes can be opened"""
     if not boxes:
         return False
 
     n = len(boxes)
-    visited = [False] * n
-    stack = [0]
+    opened = [False] * n
+    opened[0] = True  # The first box is open by default
 
-    while stack:
-        current_box = stack.pop()
-        visited[current_box] = True
+    keys = [0]  # Start with the keys in the first box
 
+    while keys:
+        current_box = keys.pop()
         for key in boxes[current_box]:
-            if key >= 0 and key < n and not visited[key]:
-                stack.append(key)
+            if key >= 0 and key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
 
-    return all(visited)
+    return all(opened)
